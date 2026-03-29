3683b362ee594fe1d871f0921f2973c576e9f86b
zxcpisya <mamin.papa0@gmail.com>
2026-03-29 19:00:47 +1000

    add code base

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, Response
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/hls/<path:filename>')
+def hls(filename):
+    return send_from_directory('hls', filename)
+
+@app.route('/')
+def index():
+    return send_from_directory('../frontend', 'index.html')
 
 @app.route('/metrics', methods=['POST'])
 def metrics():
@@ -16,4 +26,4 @@ def connect():
     print("client connected")
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000)
+    socketio.run(app, host="127.0.0.1", port=5001)
--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,8 @@
 import requests
 
-response = requests.post("http://127.0.0.1:5000/metrics", json={
+response = requests.post("http://127.0.0.1:5001/metrics", json={
   "people": 20, 
   "fps": 10
 })
 
-print(response)
+print(response.text)
